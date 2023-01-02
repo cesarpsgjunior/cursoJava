13dab10a624d3a5b71e4980222e79f7234457937
cesarpsgjunior <87958507+cesarpsgjunior@users.noreply.github.com>
2023-01-02 01:01:48 -0300

    Update exercicioSecao4.java

--- Secao4/exercicioSecao4.java
+++ Secao4/exercicioSecao4.java
@@ -93,23 +93,23 @@ public class exercicioSecao4 {
 		valUnit1 = sc.nextDouble();
 		
 		System.out.println("Digite o código da peça, quantidade e valor unitario com ponto");
-		 * codigoPeca2 = sc.nextInt(); numPecas2 = sc.nextInt(); valUnit2 =
-		 * sc.nextDouble();
-		 * 
-		 * 
-		 * System.out.println("Valor a pagar: R$ " + (double)(numPecas1 * valUnit1 +
-		 * numPecas2 * valUnit2));
-		 */
+		codigoPeca2 = sc.nextInt(); 
+		numPecas2 = sc.nextInt(); 
+		valUnit2 = sc.nextDouble();
+		 
+		System.out.println("Valor a pagar: R$ " + (double)(numPecas1 * valUnit1 + numPecas2 * valUnit2));
+		
 		// -------------------------------------------------------------------------------------//
 
-		/*
-		 * Fazer um programa que leia três valores com ponto flutuante de dupla
-		 * precisão: A, B e C. Em seguida, calcule e mostre: a) a área do triângulo
-		 * retângulo que tem A por base e C por altura. b) a área do círculo de raio C *
-		 * (pi = 3.14159) c) a área do trapézio que tem A e B por bases e C por altura.
-		 * d) a área do quadrado que tem lado B. e) a área do retângulo que tem lados A
-		 * e B.
-		 */
+		
+		//Fazer um programa que leia três valores com ponto flutuante de dupla
+		//precisão: A, B e C. Em seguida, calcule e mostre: a) a área do triângulo
+		//retângulo que tem A por base e C por altura. b) a área do círculo de raio C *
+		//(pi = 3.14159) c) a área do trapézio que tem A e B por bases e C por altura.
+		//d) a área do quadrado que tem lado B. e) a área do retângulo que tem lados A
+		//e B.
+		
+		
 
 		double A, B, C, pi = 3.14159;
 
